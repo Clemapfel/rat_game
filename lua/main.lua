1c2604dbf107c48894410744b62f2f1c90ffba52
Clem Cords <mail@clemens-cords.com>
2023-09-05 01:06:46 +0200

    signal component done

--- lua/main.lua
+++ lua/main.lua
@@ -3,19 +3,13 @@ package.path = package.path .. ";" .. RESOURCE_PATH .. "/src/?.lua"
 package.path = package.path .. ";" .. RESOURCE_PATH .. "/battle/?.lua"
 
 rt = {}
+rt.test = {}
 
 require "common"
 require "meta"
 require "notify_component"
 require "signal_component"
 require "queue"
-
-rt.Button = meta.new_type("Button", function()
-    return meta.new(rt.Button)
-end)
-instance = rt.Button()
-meta.add_signal_component(instance)
-meta.add_notify_component(instance)
 
 -- ### MAIN ###
 
--- lua/src/signal_component.lua
+++ lua/src/signal_component.lua
@@ -1,16 +1,26 @@
 --- @class SignalComponent
---- @param holder object the component should control
+--- @param holder meta.Object
 rt.SignalComponent = meta.new_type("SignalComponent", function(holder)
     meta.assert_object(holder)
     return meta.new(meta.SignalComponent, {
-        instance = holder
+        _instance = holder
     })
 end)
 
+--- @brief add signal component to object
+function rt.add_signal_component(object)
+    meta.assert_object(object)
+    object.signals = rt.SignalComponent(object)
+    return object
+end
+
 --- @brief [internal] initialize named signal
-function rt.SignalComponent._init_signal(x, name)
-    if meta.is_table(getmetatable(x).signals[name]) then return end
-    getmetatable(x).signals[name] = {
+function rt.SignalComponent._init_signal(component, name)
+    local metatable = getmetatable(component._instance)
+    if meta.is_nil(metatable.signals) then
+        metatable.signals = {}
+    end
+    metatable.signals[name] = {
         is_blocked = false,
         n = 0,
         callbacks = {}
@@ -18,133 +28,136 @@ function rt.SignalComponent._init_signal(x, name)
 end
 
 --- @brief [internal] check if object has signal with given name
-function rt.SignalComponent._assert_has_signal(x, name, scope)
-    if getmetatable(x).signals[name] == nil then
-        error("[rt] In " .. meta.typeof(x) .. "." .. scope .. ": Object has no signal with name `" .. name .. "`")
+function rt.SignalComponent._assert_has_signal(component, name, scope)
+    if getmetatable(component._instance).signals[name] == nil then
+        error("[rt] In SignalComponent." .. scope .. ": Object of type `" .. meta.typeof(component._instance) .. "`has no signal with name `" .. name .. "`")
     end
+end
+
+--- @brief check if instance has a signal with given name
+function rt.SignalComponent.has_signal(component, name)
+    meta.assert_isa(component, rt.SignalComponent)
+    meta.assert_string(name)
+    return not meta.is_nil(getmetatable(component._instance).signals[name])
 end
 
 --- @brief allow for emission of a named signal
-rt.SignalComponent.add = function(this, name)
+function rt.SignalComponent.add(component, name)
+    meta.assert_isa(component, rt.SignalComponent)
     meta.assert_string(name)
-    init_signal(x, name)
+    component:_init_signal(name)
 end
 
---- @brief [internal] create signal architecture
-function meta._initialize_signals(x)
+--- @brief invoke all connected signal handlers
+function rt.SignalComponent.emit(component, name, ...)
+    meta.assert_isa(component, rt.SignalComponent)
+    meta.assert_string(name)
+    component:_assert_has_signal(name, "emit")
 
-    local mt = getmetatable(x)
-    if mt.signals ~= nil then
-        return
+    local metatable = getmetatable(component._instance)
+    local signal = metatable.signals[name]
+
+    if meta.is_nil(signal) or signal.is_blocked then return end
+    for _, callback in pairs(signal.callbacks) do
+        callback(component._instance, ...)
     end
-    mt.signals = {}
+end
 
-    local function init_signal(x, name)
-        if meta.is_table(getmetatable(x).signals[name]) then return end
-        getmetatable(x).signals[name] = {
-            is_blocked = false,
-            n = 0,
-            callbacks = {}
-        }
-    end
+--- @brief register a callback, called when signal is emitted
+--- @param name String
+--- @param callback Function With signature (Instance, ...) -> Any
+--- @return Number handler ID
+function rt.SignalComponent.connect(component, name, callback)
+    meta.assert_isa(component, rt.SignalComponent)
+    meta.assert_string(name)
+    meta.assert_function(callback)
+    component:_assert_has_signal(name, "connect")
 
-    local function assert_has_signal(x, name, scope)
-        if getmetatable(x).signals[name] == nil then
-            error("[rt] In " .. meta.typeof(x) .. "." .. scope .. ": Object has no signal with name `" .. name .. "`")
-        end
-    end
+    local signal = getmetatable(component._instance).signals[name]
+    signal.callbacks[signal.n] = callback
+    signal.n = signal.n + 1
+    return signal.n
+end
 
-    --- @brief allow for emission of a named signal
-    --- @param name String
-    x.add_signal = function(this, name)
-        meta.assert_string(name)
-        init_signal(x, name)
-    end
+--- @brief disconnect handler permanently
+function rt.SignalComponent.disconnect(component, name)
+    meta.assert_isa(component, rt.SignalComponent)
+    meta.assert_string(name)
+    component:_assert_has_signal(name, "disconnect")
 
-    --- @brief invoke all connected signal handlers
-    --- @param name String
-    --- @param vararg
-    x.emit_signal = function(this, name, ...)
-        meta.assert_string(name)
-        assert_has_signal(this, name, "emit_signal")
-        local metatable = getmetatable(this)
-        local signal = metatable.signals[name]
-        if meta.is_nil(signal) or signal.is_blocked then return end
-
-        for _, callback in pairs(signal.callbacks) do
-            callback(this, ...)
-        end
-    end
-
-    --- @brief set whether emission is blocked
-    --- @param name String
-    --- @param b Boolean
-    x.set_signal_blocked = function(this, name, b)
-        meta.assert_string(name) meta.assert_boolean(b)
-        assert_has_signal(this, name, "set_signal_blocked")
-        getmetatable(this).signals[name].is_blocked = b
-    end
-
-    --- @brief get whether emission is blocked
-    --- @param name String
-    x.get_signal_blocked = function(this, name)
-        meta.assert_string(name)
-        assert_has_signal(this, name, "get_signal_blocked")
-        return getmetatable(this).signals[name].is_blocked
-    end
-
-    --- @brief register a callback, called when signal is emitted
-    --- @param name String
-    --- @param callback Function With signature (Instance, ...) -> Any
-    --- @return Number handler ID
-    x.connect_signal = function(this, name, callback)
-        meta.assert_string(name) meta.assert_function(callback)
-        assert_has_signal(this, name, "connect_signal")
-        local signal = getmetatable(this).signals[name]
-        signal.callbacks[signal.n] = callback
-        signal.n = signal.n + 1
-        return signal.n
-    end
-
-    --- @brief reset signal handler
-    --- @param name String
-    --- @param n Number signel handler ID or list of handler IDs
-    x.disconnect_signal = function(this, name, n)
-        meta.assert_string(name)
-        assert_has_signal(this, name, "disconnect_signal")
-        local signal = getmetatable(this).signals[name]
-        if not meta.is_nil(signal) then
-            if meta.is_nil(n) then
-                return
-            elseif meta.is_number(n) then
-                signal.callbacks[n] = nil
-            elseif meta.is_table(n) then
-                for id in ipairs(n) do
-                    signal.callbacks[id] = nil
-                end
+    local signal = getmetatable(component._instance).signals[name]
+    if not meta.is_nil(signal) then
+        if meta.is_nil(n) then
+            return
+        elseif meta.is_number(n) then
+            signal.callbacks[n] = nil
+        elseif meta.is_table(n) then
+            for id in ipairs(n) do
+                signal.callbacks[id] = nil
             end
         end
     end
+end
 
-    --- @brief get handler ids
-    --- @return Table of numbers
-    x.get_signal_handler_ids = function(this, name)
-        meta.assert_string(name)
-        assert_has_signal(this, name, "get_signal_handler_ids")
-        local signal = getmetatable(this).signals[name]
-        local out = {}
-        for id, _ in pairs(signal.callbacks) do
-            out[id] = id
-        end
-        return out
+--- @brief block handler
+function rt.SignalComponent.set_is_blocked(component, name, b)
+    meta.assert_isa(component, rt.SignalComponent)
+    meta.assert_string(name)
+    component:_assert_has_signal(name, "set_is_blocked")
+    getmetatable(component._instance).signals[name].is_blocked = b
+end
+
+--- @brief check whether signal handler is not connected or currently blocked
+function rt.SignalComponent.get_is_blocked(component, name)
+    meta.assert_isa(component, rt.SignalComponent)
+    meta.assert_string(name)
+    component:_assert_has_signal(name, "get_is_blocked")
+    return getmetatable(component._instance).signals[name].is_blocked
+end
+
+--- @brief get all handler ids for given signal
+function rt.SignalComponent.get_handler_ids(component, name)
+    meta.assert_isa(component, rt.SignalComponent)
+    meta.assert_string(name)
+    component:_assert_has_signal(this, name, "get_signal_handler_ids")
+    local signal = getmetatable(component._instance).signals[name]
+    local out = {}
+    for id, _ in pairs(signal.callbacks) do
+        out[id] = id
     end
-
-    return x
+    return out
 end
 
---- @brief allow object to emit signals, use `add_signal` to add them during the types ctor
-function meta.add_signal_component(x)
-    meta.assert_object(x)
-    meta._initialize_signals(x)
-    return x
+--- @brief [internal] test signal component
+rt.test.signal_component = function()
+    
+    local instance = meta._new("Object", {})
+    local signal = "test"
+
+    rt.add_signal_component(instance)
+    instance.signals:add(signal)
+    assert(instance.signals:has_signal(signal) == true)
+
+    assert(instance.signals:get_is_blocked(signal) == false)
+    local called = false
+    instance.signals:connect(signal, function()
+        called = true
+    end)
+    assert(instance.signals:get_is_blocked(signal) == false)
+    
+    instance.signals:emit(signal)
+    assert(called)
+    
+    instance.signals:set_is_blocked(signal, true)
+    assert(instance.signals:get_is_blocked(signal) == true)
+
+    local called = false
+    instance.signals:emit(signal)
+    assert(not called)
+    instance.signals:set_is_blocked(signal, false)
+    instance.signals:disconnect(signal)
+    instance.signals:emit(signal)
+    assert(not called)
 end
+rt.test.signal_component()
+
