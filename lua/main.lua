bb98868a9536ecca41a35a9e6aefb234ad280c8c
Clem Cords <mail@clemens-cords.com>
2023-09-08 23:09:12 +0200

    stashing before super type implementation

--- lua/main.lua
+++ lua/main.lua
@@ -26,21 +26,6 @@ require "gamepad_component"
 require "mouse_component"
 require "animation"
 
-main = {}
-
-do
-    local instance = meta._new("Object")
-    meta._install_property(instance, "hash", 1234)
-
-    main[instance.hash] = instance
-    getmetatable(instance).__gc = function(self)
-        main[self.hash] = nil
-    end
-    println(serialize(main))
-end
-
-println(serialize(man))
-
 -- ### MAIN ###
 
 if DEBUG_MODE then goto exit end
@@ -59,13 +44,34 @@ end
 --- @brief draw step
 function love.draw()
 
-    local text = love.graphics.newText(love.graphics.getFont(), tostring(math.round(love.timer.getFPS())))
-    local w, h = text:getWidth(), text:getHeight()
-    love.graphics.translate(love.graphics.getWidth() - w, 0)
-    love.graphics.draw(text)
-    love.graphics.reset()
+    local text = love.graphics.newText(love.graphics.getFont(), "EEEEEEEE\nEEEEEEE")
+    local text_w = text:getWidth()
+    local text_h = text:getHeight()
 
-    love.graphics.print("test", pos_x, pos_y)
+    local x, y, orientation_rad, scale_x, scale_y, origin_offset_x, origin_offset_y, shear_x, shear_y
+    x = love.graphics.getWidth() * 0.5 - 0.5 * text_w
+    y = love.graphics.getHeight() * 0.5 - 0.5 * text_h
+
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.draw(text, x, y, orientation_rad, scale_x, scale_y, origin_offset_x, origin_offset_y, shear_x, shear_y)
+
+    love.graphics.line(x, y, x + text_w, y, x + text_w, y + text_h, x, y + text_h)
+
+    local screen_w = love.graphics.getWidth()
+    local screen_h = love.graphics.getHeight()
+
+    love.graphics.setColor(1, 0, 1, 1)
+    love.graphics.line(0.5 * screen_w, 0, 0.5 * screen_w, screen_h)
+    love.graphics.setColor(0, 1, 1, 1)
+    love.graphics.line(0, 0.5 * screen_h, screen_w, 0.5 * screen_h)
+
+    function show_fps()
+        local text = love.graphics.newText(love.graphics.getFont(), tostring(math.round(love.timer.getFPS())))
+        local w, h = text:getWidth(), text:getHeight()
+        love.graphics.setColor(1, 1, 1, 0.5)
+        love.graphics.draw(text, love.graphics.getWidth() - w, 0)
+    end
+    show_fps()
 end
 
 --- @brief shutdown
