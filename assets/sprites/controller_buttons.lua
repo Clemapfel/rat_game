3fd119aaef23211b878c2591afa47ff9388e3e79
Clem Cords <mail@clemens-cords.com>
2023-11-04 23:57:22 +0100

    added controller buttons

---
+++ assets/sprites/controller_buttons.lua
@@ -0,0 +1,17 @@
+return {
+    name = "controller_buttons",
+    width = 24,
+    height = 24,
+    animations = {
+        A = 1,
+        B = 2,
+        X = 3,
+        Y = 4,
+        plus = 5,
+        minus = 6,
+        up = 7,
+        right = 8,
+        down = 9,
+        left = 10
+    }
+}
