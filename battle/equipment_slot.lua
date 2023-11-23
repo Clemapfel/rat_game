ded16880f5db8a4d0fe4d4e1300cc4050ab3208a
Clem Cords <mail@clemens-cords.com>
2023-11-24 00:57:45 +0100

    stashing

--- battle/equipment_slot.lua
+++ battle/equipment_slot.lua
@@ -15,29 +15,29 @@ bt.EquipmentSlot = meta.new_type("EquipmentSlot", function(equipment)
         _sprite_overlay = rt.OverlayLayout(),
         _sprite_frame = rt.Frame(rt.FrameType.CIRCULAR),
 
-        _attack_indicator = rt.DirectionIndicator(),
+        _attack_indicator = rt.DirectionIndicator(rt.Direction.UP),
         _attack_backdrop = rt.Spacer(),
         _attack_overlay = rt.OverlayLayout(),
         _attack_frame = rt.Frame(rt.FrameType.CIRCULAR),
 
-        _defense_indicator = rt.DirectionIndicator(),
+        _defense_indicator = rt.DirectionIndicator(rt.Direction.UP),
         _defense_backdrop = rt.Spacer(),
         _defense_overlay = rt.OverlayLayout(),
         _defense_frame = rt.Frame(rt.FrameType.CIRCULAR),
 
-        _speed_indicator = rt.DirectionIndicator(),
+        _speed_indicator = rt.DirectionIndicator(rt.Direction.UP),
         _speed_backdrop = rt.Spacer(),
         _speed_overlay = rt.OverlayLayout(),
         _speed_frame = rt.Frame(rt.FrameType.CIRCULAR),
 
-        _hp_indicator = rt.DirectionIndicator(),
+        _hp_indicator = rt.DirectionIndicator(rt.Direction.UP),
         _hp_backdrop = rt.Spacer(),
         _hp_overlay = rt.OverlayLayout(),
         _hp_frame = rt.Frame(rt.FrameType.CIRCULAR),
 
-        _indicator_box = rt.BoxLayout(rt.Orientation.VERTICAL),
+        _indicator_box = rt.BoxLayout(rt.Orientation.HORIZONTAL),
 
-        _main = rt.SplitLayout()
+        _main = rt.BoxLayout(rt.Orientation.VERTICAL)
 
     }, rt.Drawable, rt.Widget)
 
@@ -48,26 +48,31 @@ bt.EquipmentSlot = meta.new_type("EquipmentSlot", function(equipment)
         local overlay = out["_" .. stat .. "_overlay"]
         local frame = out["_" .. stat .. "_frame"]
 
-        indicator:set_margin(rt.settings.margin_unit)
+        indicator:set_margin(2 * rt.settings.margin_unit)
 
-        overlay:set_base_child(backdrop)
-        overlay:push_overlay(indicator)
-        frame:set_child(overlay)
-        out._indicator_box:push_back(frame)
+        --overlay:set_base_child(backdrop)
+        --overlay:push_overlay(indicator)
+        --frame:set_child(overlay)
+        out._indicator_box:push_back(indicator)
 
-        frame:set_expand(false)
-        frame:set_minimum_size(sprite_size_x * 2, sprite_size_y * 2)
+        indicator:set_minimum_size(sprite_size_x, sprite_size_y)
+        indicator:set_expand(false)
     end
+    out._indicator_box:set_spacing(rt.settings.margin_unit)
+    out._indicator_box:set_vertical_alignment(rt.Alignment.END)
 
+    out._sprite_backdrop:set_color(rt.Palette.BASE)
     out._sprite_overlay:set_base_child(out._sprite_backdrop)
-    --out._sprite_aspect:set_child(out._sprite)
-    out._sprite_overlay:push_overlay(out._sprite)
+    out._sprite_aspect:set_child(out._sprite)
+    out._sprite_overlay:push_overlay(out._sprite_aspect)
     out._sprite_frame:set_child(out._sprite_overlay)
+    out._sprite_frame:set_expand(false)
+    out._sprite_frame:set_minimum_size(sprite_size_x * 4, sprite_size_y * 4)
 
-    out._indicator_box:set_horizontal_alignment(rt.Alignment.END)
-    out._sprite_frame:set_horizontal_alignment(rt.Alignment.START)
-    out._main:set_start_child(out._sprite_frame)
-    out._main:set_end_child(out._indicator_box)
+    --out._indicator_box:set_horizontal_alignment(rt.Alignment.END)
+    --out._sprite_frame:set_horizontal_alignment(rt.Alignment.START)
+    out._main:push_back(out._sprite_frame)
+    out._main:push_back(out._indicator_box)
 
     return out
 end)
