685a26c0d3a85b0b53658b665597c7ec22617d6a
Clem Cords <mail@clemens-cords.com>
2023-12-23 02:45:04 +0100

    stashing

--- assets/shaders/wing_flap.glsl
+++ assets/shaders/wing_flap.glsl
@@ -1,20 +1,33 @@
 #pragma glsl3
 
-uniform float _time;
-
-float flap(float x, float t)
+vec2 rotate_point(vec2 point, vec2 pivot, float angle_dg)
 {
-    float prefix = t > 0 ? 1 : -1;
-    x = prefix * x;
-    if (t > 0)
-        return 1 * sqrt(exp(-1 * t * x)) - 1;
-    else if (t < 0)
-        return -1 * sqrt(exp(t * x)) + 1;
-    else
-        return 0.f;
+    float angle = angle_dg * (3.14159 / 180.0);
 
+    float s = sin(angle);
+    float c = cos(angle);
+
+    point -= pivot;
+    point.x = point.x * c - point.y * s;
+    point.y = point.x * s + point.y * c;
+    point += pivot;
+
+    return point;
+}
+
+float f(float x, float t)
+{
     return 1 * sqrt(exp(-1 * t * x)) - 1;
 }
+
+float g(float x, float t)
+{
+    return -1 * sqrt(exp(t * x)) + 1;
+}
+
+// ###########
+
+uniform float _time;
 
 vec4 effect(vec4 vertex_color, Image texture, vec2 texture_coords, vec2 vertex_position)
 {
@@ -25,11 +38,14 @@ vec4 effect(vec4 vertex_color, Image texture, vec2 texture_coords, vec2 vertex_p
     pos -= vec2(0.5);
     pos *= 10;
 
-    float x = pos.x;
-    float y = pos.y;
     float t = sin(time) * 10;
 
-    if (abs(y - flap(x, t)) < 0.01)
+    bool a = abs(pos.y - f(pos.x, t)) < 0.01 || abs(pos.y - g(pos.x, t)) < 0.01;
+
+    pos = rotate_point(pos, vec2(0), t * 2);
+    bool b = abs(pos.y - f(pos.x, t)) < 0.01 || abs(pos.y - g(pos.x, t)) < 0.01;
+
+    if (a || b)
         return vec4(vec3(1), 1);
     else
         return vec4(vec3(0), 1);
