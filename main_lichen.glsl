e2a41d2821447217ef27a5ec3328f1489d214fe5
Clemapfel <mail@clemens-cords.com>
2024-04-17 19:00:21 +0200

    added lichen

---
+++ main_lichen.glsl
@@ -0,0 +1,41 @@
+#pragma language glsl3
+
+vec3 rgb_to_hsv(vec3 c)
+{
+    vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+    vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+    vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+
+    float d = q.x - min(q.w, q.y);
+    float e = 1.0e-10;
+    return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+}
+
+vec3 hsv_to_rgb(vec3 c)
+{
+    vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+}
+
+#ifdef VERTEX
+
+flat varying float _age;
+
+vec4 position(mat4 transform, vec4 vertex_position)
+{
+    return transform * vertex_position;
+}
+
+#endif
+
+#ifdef PIXEL
+
+flat varying float _age;
+
+vec4 effect(vec4 vertex_color, Image tex, vec2 texture_coords, vec2 vertex_position)
+{
+    return vec4(1);
+}
+
+#endif
