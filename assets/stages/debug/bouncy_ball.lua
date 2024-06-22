a8561f169b5f57e6c04b8100a834eea9438404a1
Clem Cords <clemens.cords@aol.com>
2024-06-23 01:27:03 +0200

    import

---
+++ assets/stages/debug/bouncy_ball.lua
@@ -0,0 +1,31 @@
+return {
+  version = "1.10",
+  luaversion = "5.1",
+  tiledversion = "1.10.2",
+  name = "bouncy_ball",
+  class = "",
+  tilewidth = 64,
+  tileheight = 64,
+  spacing = 0,
+  margin = 0,
+  columns = 8,
+  image = "../../sprites/debug/bouncy_ball.png",
+  imagewidth = 512,
+  imageheight = 64,
+  objectalignment = "unspecified",
+  tilerendersize = "tile",
+  fillmode = "stretch",
+  tileoffset = {
+    x = 0,
+    y = 0
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 64,
+    height = 64
+  },
+  properties = {},
+  wangsets = {},
+  tilecount = 8,
+  tiles = {}
+}
