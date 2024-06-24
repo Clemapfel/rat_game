bf1e2771230d9307a2957d0c4f0c6715e890c9c2
Clem Cords <mail@clemens-cords.com>
2024-06-24 19:10:48 +0400

    benchmarking is now split into update / draw

--- main.lua
+++ main.lua
@@ -20,11 +20,6 @@ love.load = function()
     scene._state_manager:start_turn()
 end
 
-rt.graphics.frame_duration = {
-    past_frames = {},
-    n_frames_saved = 144,
-    max = 0
-}
 rt.settings.show_rulers = false
 rt.settings.show_fps = true
 
@@ -33,10 +28,10 @@ love.draw = function()
     love.graphics.clear(0.8, 0.2, 0.8, 1)
 
     if rt.current_scene ~= nil then
-        rt.current_scene:draw()
+        --rt.current_scene:draw()
     end
 
-    --list:draw()
+    list:draw()
 
     if rt.settings.show_rulers == true then
         love.graphics.setLineWidth(1)
@@ -102,45 +97,103 @@ love.run = function()
         love.timer.step()
         delta = love.timer.getDelta()
 
-        local frame_duration = 0
-        local before = love.timer.getTime()
+        if rt.graphics.frame_duration == nil then
+            rt.graphics.frame_duration = {
+                n_frames = 0,
+
+                max_update_duration = 0,
+                max_draw_duration = 0,
+                max_total_duration = 0,
+
+                update_durations = {},
+                draw_durations = {},
+                total_durations = {},
+
+                format = function(str)
+                    while #str < 3 do
+                        str = "0" .. str
+                    end
+                    return str
+                end
+            }
+        end
+
+        local durations = rt.graphics.frame_duration
+        local update_duration = 0
+        local draw_duration = 0
+        local total_duration = 0
+
+        local update_before = love.timer.getTime()
         love.update(delta)
+        update_duration = love.timer.getTime() - update_before
 
         if love.graphics.isActive() then
             love.graphics.clear(love.graphics.getBackgroundColor())
             love.graphics.reset()
+            local draw_before = love.timer.getTime()
             love.draw()
-            frame_duration = frame_duration + love.timer.getTime() - before
+            local now =  love.timer.getTime()
+            draw_duration = now - draw_before
+            total_duration = now - update_before
 
             if rt.settings.show_fps == true then
                 local fps = love.timer.getFPS()
-                local frame_usage = math.round(rt.graphics.frame_duration.max / (1 / fps) * 100)
-                local label = tostring(fps) .. " (" .. string.rep("0", math.abs(3 - #tostring(frame_usage))) .. frame_usage .. "%)"
-                local margin = 3
+                local frame_duration = 1 / fps
+                local update_percentage = tostring(math.floor(durations.max_update_duration / frame_duration * 100))
+                local draw_percentage = tostring(math.floor(durations.max_draw_duration / frame_duration * 100))
+                local total_percentage = tostring(math.floor(durations.max_total_duration / frame_duration * 100))
+
+                local label = tostring(fps) .. " | " .. durations.format(update_percentage) .. "% | " ..  durations.format(draw_percentage) .. "% | " ..  durations.format(total_percentage) .. "%"
                 love.graphics.setColor(1, 1, 1, 0.75)
+                local margin = 3
                 love.graphics.print(label, math.floor(rt.graphics.get_width() - love.graphics.getFont():getWidth(label) - 2 * margin), math.floor(0.5 * margin))
             end
 
             love.graphics.present()
         end
 
-        -- store max duration of last number of frames
-        if rt.graphics.frame_duration.n_frames_saved > 180 then
-            local to_remove = rt.graphics.frame_duration.past_frames[1]
-            table.remove(rt.graphics.frame_duration.past_frames, 1)
-            rt.graphics.frame_duration.n_frames_saved = rt.graphics.frame_duration.n_frames_saved - 1
+        durations.n_frames = durations.n_frames + 1
+        if durations.n_frames > 90 and rt.settings.show_fps == true then
+            table.insert(durations.update_durations, update_duration)
+            table.insert(durations.draw_durations, draw_duration)
+            table.insert(durations.total_durations, total_duration)
 
-            if to_remove == rt.graphics.frame_duration.max then
-                local max = 0
-                for _, v in _G._pairs(rt.graphics.frame_duration.past_frames) do
-                    max = math.max(max, v)
+            local update_update = durations.update_durations[1] == durations.max_update_duration
+            local update_draw = durations.draw_durations[1] == durations.max_draw_duration
+            local update_total = durations.total_durations[1] == durations.max_total_duration
+
+            durations.n_frames = durations.n_frames - 1
+            table.remove(durations.update_durations, 1)
+            table.remove(durations.draw_durations, 1)
+            table.remove(durations.total_durations, 1)
+
+            -- only recompute new max duration if necessary
+            if update_update then durations.max_update_duration = 0 end
+            if update_draw then durations.max_draw_duration = 0 end
+            if update_total then durations.max_total_duration = 0 end
+
+            for i = 1, durations.n_frames do
+                if update_update then
+                    durations.max_update_duration = math.max(durations.max_update_duration, durations.update_durations[i])
                 end
-                rt.graphics.frame_duration.max = max
+
+                if update_draw then
+                    durations.max_draw_duration = math.max(durations.max_draw_duration, durations.draw_durations[i])
+                end
+
+                if update_total then
+                    durations.max_total_duration = math.max(durations.max_total_duration, durations.total_durations[i])
+                end
             end
+        else
+            table.insert(durations.update_durations, update_duration)
+            table.insert(durations.draw_durations, draw_duration)
+            table.insert(durations.total_durations, total_duration)
+
+            durations.max_update_duration = math.max(durations.max_update_duration, update_duration)
+            durations.max_draw_duration = math.max(durations.max_draw_duration, draw_duration)
+            durations.max_total_duration = math.max(durations.max_total_duration, total_duration)
         end
-        table.insert(rt.graphics.frame_duration.past_frames, frame_duration)
-        rt.graphics.frame_duration.n_frames_saved = rt.graphics.frame_duration.n_frames_saved + 1
-        rt.graphics.frame_duration.max = math.max(rt.graphics.frame_duration.max, frame_duration)
 
         -- force gc
         collectgarbage("collect")
