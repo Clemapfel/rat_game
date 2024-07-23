3e7aeb4373cf710d6ef54710964b1d456dabf0c1
Clem Cords <mail@clemens-cords.com>
2024-07-23 22:42:26 +0200

    added equip tooltip

--- battle/configs/equips/DEBUG_EQUIP.lua
+++ battle/configs/equips/DEBUG_EQUIP.lua
@@ -4,7 +4,7 @@ return {
     sprite_id = "orbs",
     sprite_index = 1,
 
-    description = "Equipment used for debugging",
+    description = "Equipment used for debugging. Applies $DEBUG_STATUS at the start of the battle.",
     flavor_text = "If you read this, it means you have broken beyond the limits of this game, you are now god",
 
     hp_base_offset = 15,
@@ -12,6 +12,7 @@ return {
     defense_base_offset = 1,
     speed_base_offset = 69,
 
+    hp_base_factor = 1.1,
     attack_base_factor = 2,
     defense_base_factor = 0.9,
     speed_base_factor = 1.1,
@@ -19,6 +20,6 @@ return {
     effect = function(self, holder)
         meta.assert_equip_interface(self)
         meta.assert_entity_interface(holder)
-        println("[DBG] In " .. self:get_id() .. ".effect: effect applied to `" .. holder:get_id() .. "`")
+        self:add_status("DEBUG_STATUS")
     end
 }
--- menu/verbose_info_panel_item.lua
+++ menu/verbose_info_panel_item.lua
@@ -26,7 +26,7 @@ function mn.VerboseInfoPanel.Item._font_tiny()
 end
 
 function mn.VerboseInfoPanel.Item._title(...)
-    local out = rt.Label("<b><color=FOREGROUND><u>" .. paste(...) .. "</u></color></b>", mn.VerboseInfoPanel.Item._font())
+    local out = rt.Label("<b><u>" .. paste(...) .. "</u></b>", mn.VerboseInfoPanel.Item._font())
     out:set_justify_mode(rt.JustifyMode.LEFT)
     out:realize()
     return out
@@ -64,9 +64,23 @@ function mn.VerboseInfoPanel.Item._hrule()
     return out
 end
 
+function mn.VerboseInfoPanel.Item._colon()
+    local out = rt.Label("<color=GRAY_2><b>:</b></color>")
+    out:set_justify_mode(rt.JustifyMode.LEFT)
+    out:realize()
+    return out
+end
+
 function mn.VerboseInfoPanel.Item._get_margin()
     local m = rt.settings.margin_unit
     return m, 2 * m, m
+end
+
+function mn.VerboseInfoPanel.Item._number(value, color)
+    local out = rt.Label("<color=" .. color .. "><mono>" .. value .. "</mono></color>")
+    out:realize()
+    out:set_justify_mode(rt.JustifyMode.LEFT)
+    return out
 end
 
 function mn.VerboseInfoPanel.Item:create_from_equip(equip)
@@ -80,8 +94,6 @@ function mn.VerboseInfoPanel.Item:create_from_equip(equip)
         self.spacer = self._hrule()
         self.flavor_text_label = self._flavor_text(equip:get_flavor_text())
 
-        -- TODO: stats
-
         self.content = {
             self.title_label,
             self.sprite,
@@ -89,6 +101,69 @@ function mn.VerboseInfoPanel.Item:create_from_equip(equip)
             self.spacer,
             self.flavor_text_label
         }
+
+        local function format_offset(x)
+            if x > 0 then
+                return "+" .. x
+            elseif x < 0 then
+                return "-" .. math.abs(x)
+            else
+                return "\u{00B1}" .. x -- plusminus
+            end
+        end
+
+        local function format_factor(x)
+            x = math.abs(x)
+            if x > 1 then
+                return "+" .. math.round((x - 1) * 100) .. "%"
+            elseif x < 1 then
+                return "-" .. math.round((1 - x) * 100) .. "%"
+            else
+                return "\u{00B1}0%" -- plusminus
+            end
+        end
+
+        for stat_color_label in range(
+            {"hp", "HP", "HP"},
+            {"attack", "ATTACK", "ATK"},
+            {"defense", "DEFENSE", "DEF"},
+            {"speed", "SPEED", "SPD"}
+        ) do
+            local stat = stat_color_label[1]
+            local color = stat_color_label[2]
+
+            local offset = equip[stat .. "_base_offset"]
+            if offset ~= 0 then
+                local prefix_label = rt.Label("<color=" .. color .. ">" .. stat_color_label[3] .. "</color>")
+                prefix_label:realize()
+                local colon = self._colon()
+                local value_label = self._number(format_offset(offset), color)
+
+                self[stat .. "_offset_prefix_label"] = prefix_label
+                self[stat .. "_offset_colon_label"] = colon
+                self[stat .. "_offset_value_label"] = value_label
+
+                for label in range(prefix_label, colon, value_label) do
+                    table.insert(self.content, label)
+                end
+            end
+
+            local factor = equip[stat .. "_base_factor"]
+            if factor ~= 1 then
+                local prefix_label = rt.Label("<color=" .. color .. ">" .. stat_color_label[3] .. "</color>")
+                prefix_label:realize()
+                local colon = self._colon()
+                local value_label = self._number(format_factor(factor), color)
+
+                self[stat .. "_factor_prefix_label"] = prefix_label
+                self[stat .. "_factor_colon_label"] = colon
+                self[stat .. "_factor_value_label"] = value_label
+
+                for label in range(prefix_label, colon, value_label) do
+                    table.insert(self.content, label)
+                end
+            end
+        end
 
         self.frame:realize()
     end
@@ -103,9 +178,43 @@ function mn.VerboseInfoPanel.Item:create_from_equip(equip)
         current_y = current_y + m
 
         self.description_label:fit_into(current_x, current_y, w, POSITIVE_INFINITY)
-        current_y = current_y + select(2, self.description_label:measure())
+        current_y = current_y + select(2, self.description_label:measure()) + 1 * m
 
-        current_y = current_y + m
+        local max_prefix_w = NEGATIVE_INFINITY
+        local max_value_w = NEGATIVE_INFINITY
+        for stat in range("hp", "attack", "defense", "speed") do
+            for which in range("offset", "factor") do
+                local prefix_label = self[stat .. "_" .. which .. "_prefix_label"]
+                if prefix_label ~= nil then
+                    max_prefix_w = math.max(max_prefix_w, select(1, prefix_label:measure()))
+                end
+
+                local value_label = self[stat .. "_" .. which .. "_value_label"]
+                if value_label ~= nil then
+                    max_value_w = math.max(max_prefix_w, select(1,value_label :measure()))
+                end
+            end
+        end
+
+        for which in range("offset", "factor") do
+            for stat in range("hp", "attack", "defense", "speed") do
+                local prefix_label = self[stat .. "_" .. which .. "_prefix_label"]
+                local colon_label = self[stat .. "_" .. which .. "_colon_label"]
+                local value_label = self[stat .. "_" .. which .. "_value_label"]
+
+                if value_label ~= nil then
+                    local value_w, value_h = value_label:measure()
+                    prefix_label:fit_into(current_x, current_y, POSITIVE_INFINITY)
+                    local colon_w = select(1, colon_label:measure())
+                    colon_label:fit_into(current_x + w / 2 - colon_w, current_y, POSITIVE_INFINITY)
+                    value_label:fit_into(current_x + w - value_w, current_y, POSITIVE_INFINITY)
+
+                    current_y = current_y + value_h
+                end
+            end
+        end
+
+        current_y = current_y + 2 * m
         self.spacer:fit_into(current_x, current_y, w, 0)
         current_y = current_y + select(2, self.spacer:measure()) + m
 
