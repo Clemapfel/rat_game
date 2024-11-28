6cc0aa7e43e89a1ae33223ce0feede140fb455e4
Clem Cords <mail@clemens-cords.com>
2024-11-28 01:19:44 +0100

    stashing

--- common/blood_sort.glsl
+++ common/blood_sort.glsl
@@ -25,31 +25,50 @@ layout(std430) buffer global_offsets_buffer {
 
 uniform uint n_particles;
 
+#define n_bins 256
 #define GET(pass, i) (pass % 2 == 0 ? particle_occupations[i].hash : swap[i].hash)
 
-layout (local_size_x = 64, local_size_y = 1, local_size_z = 1) in;
+layout (local_size_x = 1, local_size_y = 1, local_size_z = 1) in;
 void computemain() // 1, 1 invocations
 {
-    uint thread_x = gl_GlobalInvocationID.x;
-    uint group_x = gl_LocalInvocationID.x;
+    uint global_x = gl_GlobalInvocationID.x;
+    uint local_x = gl_LocalInvocationID.x;
+    uint group_x = gl_WorkGroupID.x;
 
-    bool is_sequential_worker = thread_x == 0 && group_x == 0;
+    bool is_sequential_worker = global_x == 0 && local_x == 0;
 
-    const uint n_bins = 256;
     const uint bitmask = 0xFFu;
+    uint local_counts[n_bins];
 
     for (uint pass = 0u; pass < 4u; pass++)
     {
         uint shift = 8 * pass;
+
+        if (global_x < n_bins) {
+            global_counts[global_x] = 0u;
+            global_offsets[global_x] = 0u;
+        }
+
+        barrier();
+
+        for (uint i = 0; i < n_bins; ++i)
+            local_counts[i] = 0u;
+
+        uint n_per_thread = n_particles / gl_NumWorkGroups.x;
+        uint start_i = global_x * n_per_thread;
+        uint end_i = min(start_i + n_per_thread, n_particles);
+
+        for (uint i = start_i; i < end_i; ++i) {
+            uint masked = (GET(pass, i) >> shift) & bitmask;
+            local_counts[masked]++;
+        }
+
+        for (uint i = 0; i < n_bins; ++i)
+            atomicAdd(global_counts[i], local_counts[i]);
+
+        barrier();
+
         if (is_sequential_worker) {
-            for (uint i = 0; i < n_bins; ++i)
-            global_counts[i] = 0u;
-
-            for (uint i = 0; i < n_particles; ++i) {
-                uint masked = (GET(pass, i) >> shift) & bitmask;
-                atomicAdd(global_counts[masked], 1u);
-            }
-
             uint sum = 0u;
             for (uint i = 0; i < n_bins; ++i) {
                 uint count = global_counts[i];
@@ -61,9 +80,9 @@ void computemain() // 1, 1 invocations
                 uint masked = (GET(pass, i) >> shift) & bitmask;
 
                 if (pass % 2 == 0)
-                swap[global_offsets[masked]] = particle_occupations[i];
+                    swap[global_offsets[masked]] = particle_occupations[i];
                 else
-                particle_occupations[global_offsets[masked]] = swap[i];
+                    particle_occupations[global_offsets[masked]] = swap[i];
 
                 global_offsets[masked]++;
             }
@@ -71,7 +90,4 @@ void computemain() // 1, 1 invocations
 
         barrier();
     }
-
-    for (int i = 1; i < n_particles; ++i)
-        particle_occupations[i] = swap[i];
 }
