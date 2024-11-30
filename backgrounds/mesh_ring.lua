c849209e258ffe641261cd4662b25de0a63c1aff
Clem Cords <mail@clemens-cords.com>
2024-11-30 20:35:43 +0100

    stashing

--- backgrounds/mesh_ring.lua
+++ backgrounds/mesh_ring.lua
@@ -4,12 +4,10 @@ rt.Background.MESH_RING = meta.new_type("MESH_RING", rt.BackgroundImplementation
         _center_x = 0,
         _center_y = 0,
         _data = {},
+        _lines = {},
 
-        _shapes = {},
-        _colors = {},
-
-        _n_outer_vertices = 16,
-        _n_rings = 10,
+        _n_outer_vertices = 8,
+        _n_rings = 4,
 
         _elapsed = 0,
         _duration = 5
@@ -32,7 +30,12 @@ end
 
 --- @override
 function rt.Background.MESH_RING:update(delta)
-    self._elapsed = self._elapsed + delta
+    if love.keyboard.isDown("space") then
+        self._elapsed = self._elapsed + delta
+    end
+
+    self._data = {}
+    self._lines = {}
 
     -- generate positions
     local positions = {}
@@ -41,77 +44,118 @@ function rt.Background.MESH_RING:update(delta)
 
     local n_rings = self._n_rings
     local n_outer_vertices = self._n_outer_vertices
-    local step = 2 * math.pi / n_outer_vertices
+    local step = 2 * math.pi / (n_outer_vertices - 1)
 
     for ring_i = 1, n_rings do
         local fraction = (ring_i - 1) / n_rings
         local ring_x_radius = x_radius * fraction
         local ring_y_radius = y_radius * fraction
 
-        local extra = 0
-        if ring_i == n_rings then extra = 1 end -- close last triangle
+        local outline = {} -- lines around the perimeter of each ring
 
         local angle_offset = (ring_i / n_rings) * 2 * math.pi / 2 + ((ring_i + 1) / n_rings) * (self._elapsed)
-        for i = 1, n_outer_vertices + extra, 1 do
+        for i = 1, n_outer_vertices, 1 do
             local position_x = 0 + math.cos((i - 1) * step + angle_offset) * ring_x_radius
             local position_y = 0 + math.sin((i - 1) * step + angle_offset) * ring_y_radius
             table.insert(positions, { position_x, position_y })
+
+            table.insert(outline, position_x)
+            table.insert(outline, position_y)
         end
+
+        table.insert(outline, outline[1])
+        table.insert(outline, outline[2])
+        table.insert(self._lines, outline)
     end
 
-    self._shapes = {}
-    local n_shapes = 0
+    local n_triangles = n_outer_vertices * (n_rings - 1)
+    local triangle_i = 0 -- sic
+
+    for outer_i = 1, n_outer_vertices do
+        local line = {}
+        for ring_i = 1, n_rings do
+            if ring_i < n_rings then
+                -- generate triangles
+                for tri in range({
+                    (ring_i - 1) * n_outer_vertices + outer_i,
+                    (ring_i - 1) * n_outer_vertices + outer_i + 1,
+                    (ring_i - 1 + 1) * n_outer_vertices + outer_i
+                }, {
+                    (ring_i - 1) * n_outer_vertices + outer_i + 1,
+                    (ring_i - 1 + 1) * n_outer_vertices + outer_i,
+                    (ring_i - 1 + 1) * n_outer_vertices + outer_i + 1
+                }) do
+                    local color = rt.lcha_to_rgba(rt.LCHA(0.8, 1, triangle_i / n_triangles, 1))
+                    for vertex_i in values(tri) do
+                        if vertex_i < sizeof(positions) then
+                            local x, y = table.unpack(positions[vertex_i])
+                            table.insert(self._data, {
+                                x, y, color.r, color.g, color.b, color.a
+                            })
+                        end
+                    end
+                end
+                triangle_i = triangle_i + 1
+            end
+
+            -- generate perpendicular lines
+            local linear_i = (ring_i - 1) * n_outer_vertices + outer_i
+            for x in values(positions[linear_i]) do
+                table.insert(line, x)
+            end
+        end
+
+        --table.insert(self._lines, line)
+    end
+
+    --[[
     for outer_i = 1, n_outer_vertices do
         for ring_i = 1, n_rings - 1 do
-            for tri in range({
+            local line = {}
+            for position_i in range(
                 (ring_i - 1) * n_outer_vertices + outer_i,
-                (ring_i - 1) * n_outer_vertices + outer_i + 1,
-                (ring_i - 1 + 1) * n_outer_vertices + outer_i
-            }, {
                 (ring_i - 1) * n_outer_vertices + outer_i + 1,
                 (ring_i - 1 + 1) * n_outer_vertices + outer_i,
                 (ring_i - 1 + 1) * n_outer_vertices + outer_i + 1
-            }) do
-
-                local shape = {}
-                for vertex_i in values(tri) do
-                    local pos = positions[vertex_i]
-                    table.insert(shape, pos[1])
-                    table.insert(shape, pos[2])
-                end
-
-                table.insert(shape, shape[1])
-                table.insert(shape, shape[2])
-                table.insert(self._shapes, shape)
-
-                n_shapes = n_shapes + 1
+            ) do
+                local position = positions[position_i]
+                table.insert(line, position[1])
+                table.insert(line, position[2])
             end
+            table.insert(self._lines, line)
         end
     end
+    ]]--
 
-    -- generate colors
-    for i = 1, n_shapes do
-        local hue = (i - 1) / n_shapes
-        local color = rt.lcha_to_rgba(rt.LCHA(0.8, 1, hue, 1))
-        table.insert(self._colors, {rt.color_unpack(color)})
+    if self._mesh == nil then
+        self._mesh = love.graphics.newMesh({
+            {name = "VertexPosition", format = "floatvec2"},
+            {name = "VertexColor", format = "floatvec4"},
+        }, self._data, rt.MeshDrawMode.TRIANGLES, rt.GraphicsBufferUsage.STREAM)
+    else
+        self._mesh:setVertices(self._data)
     end
+
+    self._is_ready = true
 end
 
 --- @override
 function rt.Background.MESH_RING:draw()
+    if self._is_ready ~= true then return end
+
+    love.graphics.setColor(0.5, 0.5, 0.5, 1)
+    love.graphics.rectangle("fill", 0, 0, love.graphics.getDimensions())
+
+    --love.graphics.setWireframe(true)
     love.graphics.push()
     love.graphics.translate(self._center_x, self._center_y)
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.draw(self._mesh)
+
     love.graphics.setLineWidth(1)
-
-    for i, shape in ipairs(self._shapes) do
-        love.graphics.setColor(table.unpack(self._colors[i]))
-        love.graphics.polygon("fill", shape)
-    end
-
     love.graphics.setColor(0, 0, 0, 1)
-    for shape in values(self._shapes) do
-        love.graphics.polygon("line", shape)
+    for line in values(self._lines) do
+        love.graphics.line(line)
     end
-
     love.graphics.pop()
 end
